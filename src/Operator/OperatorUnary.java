a59ed3704bf3cc524c9e7f5bf784c5f6edc1209f
rzaferdian <rzaferdian44@gmail.com>
2021-04-25 14:38:16 +0800

    Operator Unary

---
+++ src/Operator/OperatorUnary.java
@@ -0,0 +1,14 @@
+package Operator;
+
+public class OperatorUnary {
+    public static void main(String[] args) {
+        int i = 0;
+        i++;
+        System.out.println(i);
+        i++;
+        System.out.println(i);
+        i = i + 1;
+        System.out.println(i);
+
+    }
+}
