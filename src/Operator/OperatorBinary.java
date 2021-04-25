384a144b326ebfc34ad5ab968d1603c84b72845e
rzaferdian <rzaferdian44@gmail.com>
2021-04-25 14:29:57 +0800

    Operator Binary Aritmatika

--- src/Operator/OperatorBinary.java
+++ src/Operator/OperatorBinary.java
@@ -9,4 +9,14 @@ public class OperatorBinary {
     String nama= "Reza Ferdian";
     int jumlahSks= 24;
     double indeksprestasikumulatif = 3.89;
+
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+
+    }
 }
