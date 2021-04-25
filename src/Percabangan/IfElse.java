cb087451d084afe19e873570a366f98996993480
rzaferdian <rzaferdian44@gmail.com>
2021-04-25 15:07:36 +0800

    if else

---
+++ src/Percabangan/IfElse.java
@@ -0,0 +1,14 @@
+package Percabangan;
+
+public class IfElse {
+    public static void main(String[] args) {
+        double totalbelanja = 53000;
+        double uangdidompet = 25000;
+
+        if(uangdidompet<totalbelanja) {
+            System.out.println("DUIT KADA CUKUP");
+        }else{
+            System.out.println("DUIT CUKUP, TERIMAKASIH");
+        }
+    }
+}
