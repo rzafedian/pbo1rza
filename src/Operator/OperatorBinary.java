30c05b4d4b2627457694e5cd59d1079a3d9f3d05
rzaferdian <rzaferdian44@gmail.com>
2021-04-25 14:24:57 +0800

    Operator Binary Assignment

---
+++ src/Operator/OperatorBinary.java
@@ -0,0 +1,12 @@
+package Operator;
+
+public class OperatorBinary {
+
+    //Operator (=) sebagai Assignment/Penugasan
+    //Menempatkan nilai kedalam variable
+    //Baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama= "Reza Ferdian";
+    int jumlahSks= 24;
+    double indeksprestasikumulatif = 3.89;
+}
