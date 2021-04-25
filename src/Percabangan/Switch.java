602029cd1318e681d413472c8124212ff0e2d560
rzaferdian <rzaferdian44@gmail.com>
2021-04-25 15:25:11 +0800

    Switch

---
+++ src/Percabangan/Switch.java
@@ -0,0 +1,25 @@
+package Percabangan;
+
+public class Switch {
+    public static void main(String[] args) {
+        int JalanJalan=3;
+        switch (JalanJalan) {
+            case 1:
+                System.out.println("URANG LAPAS");
+                break;
+            case 2:
+                System.out.println("URANG ACAYAN");
+                break;
+            case 3:
+                System.out.println("UBAT NYAMUK");
+                break;
+            case 4:
+                System.out.println("DUBIL DIT");
+                break;
+            default:
+                System.out.println("MAKHLUK HALUS");
+                break;
+        }
+
+    }
+}
